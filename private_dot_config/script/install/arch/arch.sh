223fe6e9ee13cb5b35c05dcf50f82c77ddd8ff75
Mehran Hadinejad <mehranhn@protonmail.com>
2025-10-10 04:10:21 +0330

    update scripts

--- private_dot_config/script/install/arch/arch.sh
+++ private_dot_config/script/install/arch/arch.sh
@@ -24,7 +24,8 @@ sudo pacman -Sy playerctl pavucontrol htop qt5ct qt6ct imv qbittorrent proxychai
     pipewire-docs pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio wireplumber helvum noise-suppression-for-voice \
     ttf-hack ttf-font-awesome ttf-roboto inter-font ttf-fira-code ttf-hack-nerd tofi foot nwg-look \
     noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-sourcecodepro-nerd ttf-firacode-nerd ttf-iosevka-nerd \
-    hyprland hyprsunset hypridle hyprcursor hyprpaper hyprlock hyprland-qt-support hyprpicker hyprpolkitagent hyprutils hyprshot waybar
+    hyprland hyprsunset hypridle hyprcursor hyprpaper hyprlock hyprland-qt-support hyprpicker hyprpolkitagent hyprutils hyprshot waybar \
+    easyeffects calf mda.lv2 lsp-plugins-lv2 mda.lv2 yelp zam-plugins-lv2
 
 paru -S dragon-drop hyprland-per-window-layout ianny clipvault tofi grc zen-browser-bin
 
